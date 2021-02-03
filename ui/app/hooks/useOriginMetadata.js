9c898a805f1ac67d554a9c49b2377410342ffacc
Brad Decker <git@braddecker.dev>
2021-02-03 13:31:52 -0600

    add useOriginMetadata hook

---
+++ ui/app/hooks/useOriginMetadata.js
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux'
+import { getDomainMetadata } from '../selectors'
+
+export function useOriginMetadata(origin) {
+  const domainMetaData = useSelector(getDomainMetadata)
+  if (!origin) {
+    return null
+  }
+  const url = new URL(origin)
+
+  if (domainMetaData?.[origin]) {
+    return {
+      ...domainMetaData[origin],
+      origin,
+    }
+  }
+  return {
+    host: url.host,
+    name: url.hostname,
+    origin,
+  }
+}
